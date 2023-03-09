51aa13ffb3aa7b1695efd21a43ca984256284b6f
20501a05e2 <127392328+20501a05e2@users.noreply.github.com>
2023-03-09 14:59:31 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,20 @@
+function add(x,y)
+{
+    return x+y;
+}
+function sub(x,y)
+{
+    return x-y;
+}
+function mult(x,y)
+{
+    return x*y;
+}
+function div(x,y)
+{
+    return x/y;
+}
+exports.add=add;
+exports.subtract=sub;
+exports.div=div;
+exports.mult=mult;
